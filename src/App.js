d7ea707df476a1f371a4afc99c8d06e45d7f61d6
komal <kg681278@gmail.com>
2021-05-25 12:32:41 +0530

    Set location according to our choice

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React from "react";
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    position => console.log(position),
-    err => console.log(err)
-  )
-  return (
-    <div>Current Location</div>
-  )
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null, error: "" };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (GeolocationPosition) => {
+        console.log(GeolocationPosition);
+        this.setState({ lat: GeolocationPosition.coords.latitude });
+      },
+      (GeolocationPositionError) => {
+        this.setState({ error: GeolocationPositionError.message });
+      }
+    );
+  }
+  render() {
+    if (this.state.lat && !this.state.error) {
+      return <div>latitude: {this.state.lat}</div>;
+    }
+    if (this.state.error && !this.state.lat) {
+      return <div>Error: {this.state.error}</div>;
+    }
+    return(
+      <div>
+        <div class="ui segment">
+          <div class="ui active dimmer">
+            <div class="ui text loader">Loading!!</div>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
-export default App
+export default App;
