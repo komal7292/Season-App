8cecdedf58cc7a5c82e1670692f2615e6b3d9559
komal <kg681278@gmail.com>
2021-05-26 16:44:46 +0530

    Set a sort description according to our season

--- src/App.js
+++ src/App.js
@@ -1,37 +1,22 @@
 import React from "react";
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = { lat: null, error: "" };
-
+  state = {lat: null, err: ''}
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (GeolocationPosition) => {
-        console.log(GeolocationPosition);
-        this.setState({ lat: GeolocationPosition.coords.latitude });
-      },
-      (GeolocationPositionError) => {
-        this.setState({ error: GeolocationPositionError.message });
-      }
+      GeolocationPosition => this.setState({ lat: GeolocationPosition.coords.latitude }),
+      GeolocationPositionError =>this.setState({ err: GeolocationPositionError.message })
     );
   }
   render() {
     if (this.state.lat && !this.state.error) {
-      return <div>latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat}/>
     }
-    if (this.state.error && !this.state.lat) {
-      return <div>Error: {this.state.error}</div>;
+    if (this.state.err && !this.state.lat) {
+      return <div>Error: {this.state.err}</div>;
     }
-    return(
-      <div>
-        <div class="ui segment">
-          <div class="ui active dimmer">
-            <div class="ui text loader">Loading!!</div>
-          </div>
-        </div>
-      </div>
-    )
+    return <div>Loading!!!</div>
   }
 }
 export default App;
